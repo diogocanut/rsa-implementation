20798ddce0511fc90a4eee4313f4d63cde0b3cad
Diogo Canut <canut@wcore.com.br>
2018-12-17 15:57:26 -0200

    done without free interface

--- rsa.py
+++ rsa.py
@@ -1,6 +1,24 @@
-
+# -*- coding: utf-8 -*-
 import random
 
+def eh_primo(num, test_count):
+    if num == 1:
+        return False
+    if test_count >= num:
+        test_count = num - 1
+    for x in range(test_count):
+        val = random.randint(1, num - 1)
+        if pow(val, num-1, num) != 1:
+            return False
+    return True
+
+
+def generate_big_prime(n):
+    found_prime = False
+    while not found_prime:
+        p = random.randint(2**(n-1), 2**n)
+        if eh_primo(p, 1000):
+            return p
 
 def gcd(a, b):
     while b != 0:
@@ -8,33 +26,34 @@ def gcd(a, b):
     return a
 
 
-def inverso(e, m):
-    x = lasty = 0
-    lastx = y = 1
-    while m != 0:
-        q = e // m
-        e, m = m, e % m
-        x, lastx = lastx - q*x, x
-        y, lasty = lasty - q*y, y
-    return lastx
-
-
-def eh_primo(num):
-    if num == 2:
-        return True
-    if num < 2 or num % 2 == 0:
-        return False
-    for n in xrange(3, int(num**0.5)+2, 2):
-        if num % n == 0:
-            return False
-    return True
+def inverso(e, phi):
+    d = 0
+    x1 = 0
+    x2 = 1
+    y1 = 1
+    temp_phi = phi
+    
+    while e > 0:
+        temp1 = temp_phi/e
+        temp2 = temp_phi - temp1 * e
+        temp_phi = e
+        e = temp2
+        
+        x = x2- temp1* x1
+        y = d - temp1 * y1
+        
+        x2 = x1
+        x1 = x
+        d = y1
+        y1 = y
+    
+    if temp_phi == 1:
+        return d + phi
 
 
 def generate_keypair(p, q):
-    if not (eh_primo(p) and eh_primo(q)):
-        raise ValueError('Ambos numeros digitados devem ser primos.')
-    elif p == q:
-        raise ValueError('Os números digitados não podem ser iguais')
+    if p == q:
+        raise ValueError('Os números não podem ser iguais')
     n = p * q
 
     phi = (p-1) * (q-1)
@@ -48,3 +67,44 @@ def generate_keypair(p, q):
 
     d = inverso(e, phi)
     return ((e, n), (d, n))
+
+
+def encrypt(pk, plaintext):
+    key, n = pk
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+    return cipher
+
+
+def decrypt(pk, ciphertext):
+    key, n = pk
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    return ''.join(plain)
+    
+
+if __name__ == '__main__':
+
+    while True:
+        print("1 - Encrypt")
+        option = int(raw_input())
+
+        if option == 1:
+            p = generate_big_prime(7)
+            q = generate_big_prime(4)
+     
+            public, private = generate_keypair(p, q)
+            print("Chave publica: {0} Chave privada: {1}".format(public, private))
+            message = raw_input("Escreva a mensagem à ser criptografada: ")
+            encrypted_msg = encrypt(private, message)
+
+            print "A mensagem cifrada é: "
+            print ''.join(map(lambda x: str(x), encrypted_msg))
+            option = int(raw_input("2 - Decrypt"))
+
+            if option == 2:
+                print("Decifrando mensagem com chave pública {0}".format(public))
+                print("A mensagem é:")
+                print(decrypt(public, encrypted_msg))
+            else:
+                print("Digite uma opção valida")
+        else:
+            print("Digite uma opção valida")
